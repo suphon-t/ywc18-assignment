86d9bff627b5bd56e1d76240b47a2c60bb558633
paphonb <paphonb@gmail.com>
2020-11-21 00:03:43 +0700

    Add an escape hatch to force light/dark mode

--- src/providers/AppThemeProvider.tsx
+++ src/providers/AppThemeProvider.tsx
@@ -1,14 +1,19 @@
 import React, { PropsWithChildren } from 'react'
 import { useMediaQuery, createMuiTheme, ThemeProvider } from '@material-ui/core'
 import { darkThemeOptions, lightThemeOptions } from '../utils/theme'
+import { useQueryState } from '../utils'
 
 export function AppThemeProvider({ children }: PropsWithChildren<{}>) {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const [forcedColorMode] = useQueryState('forcedColorMode', 'auto')
+
+  const useDarkMode =
+    forcedColorMode === 'dark' ||
+    (prefersDarkMode && forcedColorMode !== 'light')
 
   const theme = React.useMemo(
-    () =>
-      createMuiTheme(prefersDarkMode ? darkThemeOptions : lightThemeOptions),
-    [prefersDarkMode]
+    () => createMuiTheme(useDarkMode ? darkThemeOptions : lightThemeOptions),
+    [useDarkMode]
   )
 
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>
